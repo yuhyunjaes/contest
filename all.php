bef6255eb35dc637898de28b215752341e970d26
유현재 <131428966+yuhyunjaes@users.noreply.github.com>
2025-03-24 00:00:22 +0900

    Add files via upload

---
+++ all.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="all.css">
+</head>
+<body>
+    <div id="wrap">
+        <header>
+            <div class="real_header">
+                <div class="logo">
+                    <label for="side1">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </label>
+                    <a href="web.php"><img src="image/로고4.png" alt="로고고"></a>
+                </div>
+                <nav>
+                    <ul class="menu">
+                        <li>
+                            <a href="intro.php">소개</a>
+                        </li>
+                        <li>
+                            <a href="#">판매상품</a>
+                            <ul class="submenu">
+                                <li><a href="all.php">전체상품</a></li>
+                                <li><a href="hot.php">인기상품</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">가맹점</a>
+                        </li>
+                        <li>
+                            <a href="cart.php">장바구니</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="bu_menu">
+                    <ul class="buumenu">
+                        <li>
+                            <a href="#">관리자</a>
+                        </li>
+                        <li>
+                            <a href="cart.php">장바구니</a>
+                        </li>
+                        <li>
+                            <a href="#">로그인</a>
+                        </li>
+                        <li>
+                            <a href="#">회원가입</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="contents">
+            <div class="contents_textbox">
+                <h1><span>GIFTS:Mall</span> 전체상품</h1>
+                <p><span style="font-weight: 100;color: white;">GIFTS:Mall</span> 홈페이지에 오신것을 환영합니다.</p>
+            </div>
+        </div>
+        <div class="all_container">
+        </div>
+    </div>
+</body>
+</html>
---
+++ intro.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="intro.css">
+    <script src="js/intro.js"></script>
+</head>
+<body>
+    <div id="wrap">
+    <header>
+            <div class="real_header">
+                <div class="logo">
+                    <label for="side1">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </label>
+                    <a href="web.php"><img src="image/로고4.png" alt="로고고"></a>
+                </div>
+                <nav>
+                    <ul class="menu">
+                        <li>
+                            <a href="intro.php">소개</a>
+                        </li>
+                        <li>
+                            <a href="#">판매상품</a>
+                            <ul class="submenu">
+                                <li><a href="all.php">전체상품</a></li>
+                                <li><a href="hot.php">인기상품</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">가맹점</a>
+                        </li>
+                        <li>
+                            <a href="cart.php">장바구니</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="bu_menu">
+                    <ul class="buumenu">
+                        <li>
+                            <a href="#">관리자</a>
+                        </li>
+                        <li>
+                            <a href="cart.php">장바구니</a>
+                        </li>
+                        <li>
+                            <a href="#">로그인</a>
+                        </li>
+                        <li>
+                            <a href="#">회원가입</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="contents">
+            <div class="contents_textbox">
+                <h1><span>GIFTS:Mall</span> 소개</h1>
+                <p><span style="font-weight: 100;color: white;">GIFTS:Mall</span> 홈페이지에 오신것을 환영합니다.</p>
+            </div>
+        </div>
+        <div class="banner_container">
+            <div class="banner_content1">
+                <div class="banner_background"></div>
+                <div class="banner_text">
+                    <div class="ba_text">
+                        <p>고객과 가장 가까운 곳에서 정(情)을 전달합니다. <br>
+우리는 100여개의 매장을 기반으로 고객에게 상품을 큐레이션합니다.<br>
+또한 체험형 콘텐츠와 커머스의 연계를 통해 파트너사들에게 매력적인 브랜딩, 마케팅 채널로 진화하고 있습니다.</p>
+                        <a href="#">자세히 알아보기 ></a>
+                    </div>
+                </div>
+            </div>
+            <div class="banner_content2">
+                <div class="banner_text">
+                    <div class="ba_text">
+                        <p>고객의 니즈를 반영하는 온라인 PLAYGROUND를 제공합니다. <br>
+우리는 차별화된 온라인 콘텐츠와 AI기술 기반 상품 추천을 통해 월 이용자 수 480만명, 멤머십 회원 수 1,400만명 이상의 선물 커뮤니티를 구현했습니다.<br>
+또한 업계 최초 당일 배송 서비스인 오늘드림을 실행하여 고객경험을 혁신했습니다.</p>
+                        <a href="#">자세히 알아보기 ></a>
+                    </div>
+                </div>
+                <div class="banner_background"></div>
+            </div>
+            <div class="banner_content3">
+                <div class="banner_background"></div>
+                <div class="banner_text">
+                    <div class="ba_text">
+                        <p>
+                        전 세계에 Made in Korea 브랜드를 알립니다.  <br>
+우리는 Made in Korea 브랜드들이 세계로 뻗어 나가는 교두보 역할을 수행합니다.<br>
+2024년 런칭한 글로벌몰과 연간 약 300만명 이상이 외국인 고객이 방문하는 국내 글로벌 특화매장은 파트너사와 함께 글로벌 시장에서 동반 성장을 할 수 있는 핵심 원동력입니다.
+                        </p>
+                        <a href="#">자세히 알아보기 ></a>
+                    </div>
+                </div>
+            </div>
+            <div class="banner_content4">
+                <div class="banner_text">
+                    <div class="ba_text">
+                        <p>
+                        가치지향 라이프스타일 플랫폼을 통해 고객에게 다양한 즐거움을 선물합니다. <br>
+고객이 라이프스타일 전반에서 자신만의 가치와 취향을 반영한 브랜드를 탐색할 수 있습니다.<br>
+이러한 고객 경험의 확장을 통해 우리는 종합 라이프스타일 플랫폼으로 도약합니다.
+                        </p>
+                        <a href="#">자세히 알아보기 ></a>
+                    </div>
+                </div>
+                <div class="banner_background"></div>
+            </div>
+        </div>
+        <div class="motto_container">
+            <div class="motto_box">
+                <div id="motto1" class="motto" style="--backposition: 0%;">    
+                    <div class="motto_text">
+                        <h1>행복신뢰</h1>
+                    </div>
+                </div>
+                <div id="motto2" class="motto" style="--backposition: -240px;">
+                    <div class="motto_text">
+                        <h1>가치나눔</h1>
+                    </div>
+                </div>
+                <div id="motto3" class="motto" style="--backposition: -480px;">
+                    <div class="motto_text">
+                        <h1>환경선도</h1>
+                    </div>
+                </div>
+                <div id="motto4" class="motto" style="--backposition: -720px;">
+                    <div class="motto_text">
+                        <h1>미래혁신</h1>
+                    </div>
+                </div>
+                <div id="motto5" class="motto" style="--backposition: -960px;">
+                    <div class="motto_text">
+                        <h1>정보보안</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="real_footer">
+                <div class="sns">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div class="dae1">
+                주말 및 공휴일은 1:1문의하기를 이용해주세요. <br>
+                업무가 시작되면 바로 처리해드립니다. <br><br>
+                지방은행구매안전서비스<br>
+GIFTS:Mall은 현금 결제한 금액에 대해 지방은행과 채무지급보증 <br> 계약을체결하여 안전한 거래를 보장하고 있습니다.<br>
+<a href="#">서비스 가입사실 확인 ></a>
+                </div>
+                <ul class="footer_menu">
+                    <li><a href="#">개인정보처리방침</a></li>
+                    <li>|</li>
+                    <li><a href="#">이용약관.법적고지</a></li>
+                    <li>|</li>
+                    <li><a href="#">청소년보호방침</a></li>
+                    <li>|</li>
+                    <li><a href="#">이메일무단수집거부</a></li>
+                    <li>|</li>
+                    <li><a href="#">사이트맵</a></li>
+                    <li>|</li>
+                    <li><a href="#">채용</a></li>
+                </ul>
+                <div class="footer_logo">
+                    <a href="#"><img src="image/로고4.png" alt=""></a>
+                </div>
+                <div class="work_text">
+                    <p>(주)GIFTS:Mall | 사업자등록번호 : 809-81-01157 | 대표이사 황기영 <br>
+주소 : 서울특별시 용산구 한강대로 123, 40층 <br>
+본사 대표전화 : 02-123-4567 | GIFTS:Mall 가맹상담전화 : 02-123-4568<br> <br>
+고객센터 이용안내 <br>
+- 온라인몰 고객센터 1580-8282<br>
+- 매장고객센터 1577-8254 <br><br>고객센터 운영시간 [평일 09:00 - 18:00]
+</p>
+                </div>
+                <div class="copyright">
+                COPYRIGHTⓒ 2024 GIFTS:MALL KOREA INC. ALL RIGHTS RESERVED
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
